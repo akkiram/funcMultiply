982f5ca4d67ecfab1a6e860307de6c16ac06ce74
akkiram <114799941+akkiram@users.noreply.github.com>
2022-11-09 17:41:54 +0530

    python
    python file

---
+++ funcMultiply.py
@@ -0,0 +1,24 @@
+#def multiply(a,b):
+ #   multiply = a*b
+  #  return multiply
+
+#x = multiply(3,0)
+#print(x)
+
+
+#def division(a,b):
+ #   division = a%b
+  #  print(division)
+
+#division(80,5)
+
+
+#def name(firstname,secondname):
+ #   print("my name is",firstname + " " + secondname)
+
+#name("Ram","Sriram")
+
+def name(name,*myself):
+    print(name)
+    print(myself)
+name("ram","harsh","mehoroj")
